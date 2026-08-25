ac1249d8ca84f52edbdd2119ecc6befca5442839
odogiyonemmanuel-lab <odogiyonemmanuel@gmail.com>
2026-08-25 11:41:33 +0100

    Create index.ts to export admin components

---
+++ src/components/admin/index.ts
@@ -0,0 +1,5 @@
+export { default as AdminDashboard } from "./AdminDashboard";
+export { default as AdminUsers } from "./AdminUsers";
+export { default as AdminTransactions } from "./AdminTransactions";
+export { default as AdminPricing } from "./AdminPricing";
+export { default as AdminWallet } from "./AdminWallet";
