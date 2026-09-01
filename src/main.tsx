1edf62e80c8c321c4a28c261b0442b5b51d47019
odogiyonemmanuel-lab <odogiyonemmanuel@gmail.com>
2026-09-01 18:17:20 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error('Root element #root was not found.');
 }
 
 createRoot(rootElement).render(
